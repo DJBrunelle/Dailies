6a1c44f642d19dc998b61bc7a342b29dddcee6d7
Daniel Brunelle <daniel.brunelle@me.com>
2022-04-25 14:49:40 -0600

    Day 1 P1 Complete

--- aoc/2021/day1/SonarSweep.cpp
+++ aoc/2021/day1/SonarSweep.cpp
@@ -1,7 +1,53 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+bool IsLarger(int,int);
+
+/*
+    count the number of times a depth measurement increases from the previous measurement in a given file
+*/
 
 int main()
 {
+    ifstream file;
+    file.open("depths.txt");
+
+    int prev = -1;
+
+    int increaseCount = 0;
+
+    if (file.is_open())
+    {
+        string line;
+        while (file)
+        {
+            getline(file,line);
+
+            int next = stoi(line);
+
+            if (IsLarger(prev, next))
+            {
+                increaseCount++;
+            }
+
+            prev = next;
+        }
+    }
+
+    file.close();
+
+    cout << increaseCount;
     
     return 0;
+}
+
+bool IsLarger(int prev, int next)
+{
+    if (next > prev && prev != -1)
+    {
+        return true;
+    }
+    return false;
 }
